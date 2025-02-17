693b2383996654e5c76d015b769cff8556a8728b
Aysenur Tatli <aysenurtatlii@hotmail.com>
2025-02-17 15:27:33 +0300

    footer changes

--- src/parts/Footer/index.jsx
+++ src/parts/Footer/index.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import logo from '../../assets/images/logo.png'
+import { Link } from 'react-router';
 import { FaInstagram, FaFacebook } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import { navLinks } from '../Navbar/navLinks';
 
 function Footer() {
+
     return (
         <footer className='bg-[#CCE0A9]'>
-            <div className='container flex items-center justify-between p-8 mx-auto'>
-                <img src={logo} alt="logo" className='w-[100px]' />
-                <div className='text-stone-800'>
-                    <ul className='flex items-center gap-5 text-2xl'>
-                        <li><FaInstagram /></li>
-                        <li><FaFacebook /></li>
-                        <li><FaXTwitter /></li>
+            <div className='container flex flex-col justify-center items-center py-[5rem] mx-auto'>
+                <div className='flex w-full  justify-center items-center'>
+                    <ul className='flex flex-col w-full justify-center gap-20 md:flex-row items-center text-center'>
+                        {navLinks.map((link) => (
+                            <li><Link>{link.tag}</Link></li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className='flex items-center m-10 gap-10'>
+                    <img src={logo} alt="logo" className='w-[100px]' />
+                    <ul className='flex items-center text-[#0f4831] gap-10 text-2xl'>
+                        <li><a href="#"><FaInstagram /></a></li>
+                        <li><a href="#"><FaFacebook /></a></li>
+                        <li><a href="#"><FaXTwitter /></a></li>
                     </ul>
                 </div>
             </div>
