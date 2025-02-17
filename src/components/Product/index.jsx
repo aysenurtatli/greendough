0427b1ad23610c8ff1d3c799bb3c0bf662b895c9
Aysenur Tatli <aysenurtatlii@hotmail.com>
2025-02-17 15:31:22 +0300

    add link to image and text

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import { productInfos } from './productInfos'
 import Button from '../Button'
+import { Link } from 'react-router'
 function Product() {
 
     return (
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
             {productInfos.map((product) => (
                 <div className='flex flex-col items-center justify-center'>
-                    <div className='max-w-sm bg-white rounded-4xl'>
-                        <img src={product.imageSrc} alt={product.name} className='w-full' />
-                    </div>
-                    <div className='my-6'>
-                        <div className='flex items-center justify-center gap-3'>
+                    <Link>
+                        <div className='max-w-xs bg-white rounded-4xl'>
+                            <img src={product.imageSrc} alt={product.name} className='w-full' />
+                        </div>
+                    </Link>
+                    <div className='my-4'>
+                        <Link className='text-xs lg:text-sm hover:text-[#FF9D67] duration-200'>GreenDough Oyun hamuru - {product.name}</Link>
+                        <div className='flex items-center justify-center gap-3 my-2'>
                             <span>74.90 TL</span>
                             <Button children="Sepete Ekle" type='button' size='sm' variant='primary' />
                         </div>
-                        <span className='text-sm'>GreenDough Oyun hamuru - {product.name}</span>
                     </div>
                 </div>
             ))}
