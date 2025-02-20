5dda1afa147274a0d5d743f7e4def69f95df4e74
Aysenur Tatli <aysenurtatlii@hotmail.com>
2025-02-20 11:16:40 +0300

    remove unnecesarry elements

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,24 +1,19 @@
 import React from 'react'
 import { productInfos } from './productInfos'
-import Button from '../Button'
 import { Link } from 'react-router'
 function Product() {
 
     return (
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
             {productInfos.map((product) => (
-                <div className='flex flex-col items-center justify-center'>
+                <div>
                     <Link>
-                        <div className='max-w-xs bg-white rounded-4xl'>
-                            <img src={product.imageSrc} alt={product.name} className='w-full' />
+                        <div className=' bg-white shadow-lg shadow-[#e7dad2] rounded-4xl'>
+                            <img src={product.imageSrc} alt={product.name} className='w-full duration-300 ease-in-out hover:rotate-10 hover:scale-110' />
                         </div>
                     </Link>
-                    <div className='my-4'>
-                        <Link className='text-xs lg:text-sm hover:text-[#FF9D67] duration-200'>GreenDough Oyun hamuru - {product.name}</Link>
-                        <div className='flex items-center justify-center gap-3 my-2'>
-                            <span>74.90 TL</span>
-                            <Button children="Sepete Ekle" type='button' size='sm' variant='primary' />
-                        </div>
+                    <div className='my-4 text-center'>
+                        <Link className='text-xs lg:text-sm duration-200'>GreenDough Oyun hamuru - {product.name}</Link>
                     </div>
                 </div>
             ))}
