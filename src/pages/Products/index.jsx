f10db1fc071a44db2e6ec8a2a816afef45ca7581
Aysenur Tatli <aysenurtatlii@hotmail.com>
2025-02-20 11:17:17 +0300

    change container width

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Product from '../../components/Product'
 function Products() {
     return (
-        <div className='max-w-6xl mx-auto p-4'>
-            <h2 className='text-center text-xl sm:text-2xl md:text-3xl font-medium mb-10'>TEKLİ OYUN HAMURLARI</h2>
+        <div className='container mx-auto p-4 min-h-screen'>
+            <h2 className='text-center text-xl sm:text-2xl md:text-3xl font-medium mb-10 text-[#327E60]' style={{ fontFamily: "dynaPuff" }}>TEKLİ OYUN HAMURLARI</h2>
             <Product />
         </div>
     )
