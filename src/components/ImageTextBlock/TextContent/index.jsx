4aedda53af1956627ade9ea5ef5cbff7268ebef6
Aysenur Tatli <aysenurtatlii@hotmail.com>
2025-02-20 11:16:19 +0300

    change title size

--- src/components/ImageTextBlock/TextContent/index.jsx
+++ src/components/ImageTextBlock/TextContent/index.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 function TextContent({
     title,
     text,
-    titleSize = "2xl",
+    titleSize = { lg: "3xl", md: "2xl", sm: "xl" },
     titleColor = "text-[#00805F]",
     textColor = "text-[#1f1f1f]",
     textSize = "sm",
 }) {
     return (
         <div>
-            <h3 className={`text-${titleSize} font-medium ${titleColor}`} style={{ fontFamily: "DynaPuff" }}>{title}</h3>
+            <h3 className={`sm:text-${titleSize.sm} md:text-${titleSize.md} lg:text-${titleSize.lg} font-medium ${titleColor}`} style={{ fontFamily: "DynaPuff" }}>{title}</h3>
             <p className={`text-${textSize} ${textColor}  md:text-md lg:text-lg`}>{text}</p>
         </div>
     )
