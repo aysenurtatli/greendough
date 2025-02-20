c89a8fa22a454218cfefd680ce8b36b9cf06b346
Aysenur Tatli <aysenurtatlii@hotmail.com>
2025-02-20 11:16:08 +0300

    add shadow to product image

--- src/components/ImageTextBlock/Image/index.jsx
+++ src/components/ImageTextBlock/Image/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Image({ imageSrc, altText, rounded = "xl" }) {
     return (
-        <img src={imageSrc} alt={altText} className={`w-full rounded-${rounded} `} />
+        <img src={imageSrc} alt={altText} className={`w-full shadow-lg shadow-[#e7dad2] rounded-3xl`} />
     )
 }
 
