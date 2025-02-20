75ed66b6c39c69f46f4c57bf7198ec5a983d258a
Aysenur Tatli <aysenurtatlii@hotmail.com>
2025-02-20 11:17:26 +0300

    footer ui changes

--- src/parts/Footer/index.jsx
+++ src/parts/Footer/index.jsx
@@ -9,21 +9,21 @@ function Footer() {
 
     return (
         <footer className='bg-[#CCE0A9]'>
-            <div className='container flex flex-col justify-center items-center py-[5rem] mx-auto'>
+            <div className='container flex flex-col justify-center items-center h-[400px] md:h-[250px] mx-auto gap-10'>
                 <div className='flex w-full  justify-center items-center'>
-                    <ul className='flex flex-col w-full justify-center gap-20 md:flex-row items-center text-center'>
-                        {navLinks.map((link) => (
-                            <li><Link>{link.tag}</Link></li>
+                    <ul className='flex flex-col w-full justify-center gap-20  md:flex-row items-center text-sm text-center text-[#644b32]'>
+                        {navLinks.map((link, index) => (
+                            <li key={index}><Link to={link.to}>{link.tag}</Link></li>
                         ))}
                     </ul>
                 </div>
 
-                <div className='flex items-center m-10 gap-10'>
-                    <img src={logo} alt="logo" className='w-[100px]' />
-                    <ul className='flex items-center text-[#0f4831] gap-10 text-2xl'>
-                        <li><a href="#"><FaInstagram /></a></li>
-                        <li><a href="#"><FaFacebook /></a></li>
-                        <li><a href="#"><FaXTwitter /></a></li>
+                <div className='flex items-center gap-10'>
+                    <img src={logo} alt="logo" className='w-[80px] md:w-[100px]' />
+                    <ul className='flex items-center text-[#0f4831] gap-10 text-xl md:text-2xl'>
+                        <li className='bg-[#FDF5F0] border-2 shadow-lg shadow-[#afc79b] border-white rounded-full w-8 h-8 md:w-10 md:h-10  flex items-center justify-center'><a href="#"><FaInstagram /></a></li>
+                        <li className='bg-[#FDF5F0] border-2 shadow-lg shadow-[#afc79b] border-white rounded-full w-8 h-8 md:w-10 md:h-10 flex items-center justify-center'><a href="#"><FaFacebook /></a></li>
+                        <li className='bg-[#FDF5F0] border-2 shadow-lg shadow-[#afc79b] border-white rounded-full w-8 h-8 md:w-10 md:h-10 flex items-center justify-center'><a href="#"><FaXTwitter /></a></li>
                     </ul>
                 </div>
             </div>
