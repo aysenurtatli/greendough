0f63bbb7c1b0b71fc7f32ad7210b2abba1ac69d1
Aysenur Tatli <aysenurtatlii@hotmail.com>
2025-02-20 11:17:37 +0300

    remove unnecesarry elements

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import Home from './pages/Home'
 import Footer from './parts/Footer'
 import About from './pages/About'
 import Products from './pages/Products'
-import Contact from './pages/Contact'
-
 
 function App() {
 
@@ -14,10 +12,9 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />}></Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path='/about' element={<About />}></Route>
         <Route path='/products' element={<Products />}></Route>
-        <Route path='/contact' element={<Contact />}></Route>
       </Routes>
       <Footer />
     </>
